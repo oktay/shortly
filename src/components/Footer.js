3022599ed4eea9379139610b686bb25bf4959904
oktay <oktaycolakoglu@gmail.com>
2021-07-24 21:14:56 +0300

    fix no-used-vars error

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import { Box, Container, Divider, Flex, Link, Stack, Text } from "@chakra-ui/react"
+import { Box, Container, Flex, Link, Stack, Text } from "@chakra-ui/react"
 
 function Footer() {
   return (
